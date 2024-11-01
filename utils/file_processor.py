8c7c840f42d5c893b1378eda0713f61da6c846b3
miki <michelle.lee6@mail.mcgill.ca>
2024-10-31 23:34:26 -0400

    kinda works

--- utils/file_processor.py
+++ utils/file_processor.py
@@ -16,6 +16,21 @@ import os
 #     except Exception as e:
 #         print(f'Error processing file {filepath}: {e}')
 
+mentor_replacement_mapping = {
+    "First": "mentor_name",
+    "Major": "engineering",
+    "language": "mentor_language",
+    "student": "max_mentee"
+}
+
+mentee_replacement_mapping = {
+    "Last": "mentee_name",
+    "Major": "interest",
+    "language": "language",
+    "Email Address": "email",
+    "expect": "expectations"
+}
+
 def process_mentor_files(filepath):
     try:
         # Implement processing logic for mentor spreadsheet
@@ -23,6 +38,10 @@ def process_mentor_files(filepath):
         df = pd.read_excel(filepath)
         print(f'Headers of {filepath}: {df.columns.tolist()}')
         # Add specific processing steps for mentor spreadsheet
+        for column_name in filepath.columns:
+            for word_to_find, new_word in mentor_replacement_mapping.items():
+                if word_to_find in column_name:
+                    filepath.rename(columns={column_name: column_name.replace(column_name, new_word)}, inplace=True)
     except Exception as e:
         print(f'Error processing file {filepath}: {e}')
 
